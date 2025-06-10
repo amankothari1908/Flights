8dcd5c836cbdc63d76245f891dac868e07ec7b0f
Aman <aman@Amans-MacBook-Air.local>
2025-06-10 10:11:41 +0530

    basic setup and few API added for getALl , get and create Airplane

---
+++ src/services/airplane.service.js
@@ -0,0 +1,59 @@
+const { StatusCodes } = require("http-status-codes");
+const AirplaneRepository = require("../repositories/airplane.repository");
+const AppError = require("../utils/errors/app.error");
+
+const airplaneRepository = new AirplaneRepository();
+
+async function createAirplane(data) {
+  try {
+    const airplane = await airplaneRepository.create(data);
+    return airplane;
+  } catch (error) {
+    if (error.name == "TypeError") {
+      throw new AppError(
+        "Cannot create a new Airplane object",
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+    throw new AppError(
+      "Cannot create a new airplane object",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function getAirplanes() {
+  try {
+    const airplanes = await airplaneRepository.getAll();
+    return airplanes;
+  } catch (error) {
+    throw new AppError(
+      "Cannot fetch the data of all the airplane",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function getAirplane(id) {
+  try {
+    const airplane = await airplaneRepository.get(id);
+    return airplane;
+  } catch (error) {
+    if (error.statusCode === StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        `The Airplane you request is not present for id : ${id}`,
+        error.statusCode
+      );
+    }
+    throw new AppError(
+      `Cannot fetch the data of the airplane with id : ${id}`,
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+module.exports = {
+  createAirplane,
+  getAirplanes,
+  getAirplane,
+};
